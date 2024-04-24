46d6ffcf2a0cebe77e3d1bb3a0eae043e23bdd7f
LooPyt <luis.schmid@uzh.ch>
2024-04-24 03:46:36 +0200

    working with the client for the chat

--- src/main/java/ch/uzh/ifi/hase/soprafs24/config/WebSocketConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/config/WebSocketConfig.java
@@ -23,7 +23,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("http://localhost:3000").withSockJS();
     }
 
     @Bean
