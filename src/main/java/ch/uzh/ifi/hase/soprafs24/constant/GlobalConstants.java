6e473b80f825d245fd930403422243e43fc0dbfc
Davtan00 <david.tanner2@uzh.ch>
2024-04-26 19:12:01 +0200

    env stuff out

--- src/main/java/ch/uzh/ifi/hase/soprafs24/constant/GlobalConstants.java
+++
@@ -1,9 +0,0 @@
-package ch.uzh.ifi.hase.soprafs24.constant;
-
-public class GlobalConstants {
-
-    public static final Long MAX_REQUEST_DURATION = 60L;
-
-    public static final Long POLL_TIMEOUT = 5000L;
-    
-}
