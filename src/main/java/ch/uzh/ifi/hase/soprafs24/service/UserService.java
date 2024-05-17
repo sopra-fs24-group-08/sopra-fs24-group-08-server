c2e9cdff4b181d554a8970790a898863498b1706
Davtan00 <david.tanner2@uzh.ch>
2024-05-17 16:05:00 +0200

    IMPLEMENT THIS IN MAIN,ONLINE/OFFLINE login check

--- src/main/java/ch/uzh/ifi/hase/soprafs24/service/UserService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/service/UserService.java
@@ -134,6 +134,10 @@ public class UserService {
         if (!password.equals(savedPassword)) {
             throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, String.format(passwordErrorMessage));
         }
+
+        if(userByUsername.getStatus()==UserStatus.ONLINE) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "An account with these credentials is already logged in");
+        }
         userByUsername.setStatus(UserStatus.ONLINE);
         return userByUsername;
     }
